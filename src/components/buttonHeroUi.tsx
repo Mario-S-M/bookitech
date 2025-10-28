52b0199c1a0bb2c0756cc8a7b66a2736f592df56
Mario Eduardo Sánchez Mejía <mayitolalito@hotmail.com>
2025-10-28 12:27:03 -0600

    First commit

---
+++ src/components/buttonHeroUi.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@heroui/react";
+
+export default function MyButton() {
+  return (
+    <Button color="primary">My button</Button>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { heroui } from "@heroui/react";
+
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
